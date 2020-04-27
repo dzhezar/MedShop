3e7b631d7096dcbd9bf3cd5a52d508306504d9b6
dzhezar <dzhezik@gmail.com>
2020-04-27 22:12:34 +0300

    swiper blocked when cart is open

--- public/js/index_page.js
+++ public/js/index_page.js
@@ -11,6 +11,14 @@ $(document).ready(function () {
             loadPrevNext: true,
         },
     });
+    $('.cart-trigger').on('click',function () {
+        swiper.allowTouchMove = false;
+        swiper.update();
+    })
+    $('header .cart-wrapper .cart .back_to_shopping, .shadow-filter').on('click', function () {
+        swiper.allowTouchMove = true;
+        swiper.update();
+    })
     $('.lazy').Lazy();
     $('.menuOpen').on('click',function () {
         $('body').toggleClass('hidden');
