95855cf9fab2357529c08c12167a005153e30fee
eugenpopov <zheny1995q@gmail.com>
2020-05-10 16:08:12 +0300

    Added history page rendering

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
+use App\Repository\OrdersRepository;
 use App\Repository\StateRepository;
 use App\Service\CartService;
+use App\Service\OrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,27 +20,39 @@ class CheckoutController extends AbstractController
      * @var StateRepository
      */
     private $stateRepository;
+    /**
+     * @var OrderService
+     */
+    private $orderService;
 
     /**
      * CheckoutController constructor.
      * @param CartService $cartService
      * @param StateRepository $stateRepository
+     * @param OrderService $orderService
      */
-    public function __construct(CartService $cartService, StateRepository $stateRepository)
-    {
+    public function __construct(
+        CartService $cartService,
+        StateRepository $stateRepository,
+        OrderService $orderService
+    ) {
         $this->cartService = $cartService;
         $this->stateRepository = $stateRepository;
+        $this->orderService = $orderService;
     }
 
     public function history(Request $request)
     {
-        return $this->render('checkout/history.html.twig');
+        return $this->render(
+            'checkout/history.html.twig',
+            ['orders' => $this->orderService->getAllByCookies($request)]
+        );
     }
 
     public function index(Request $request)
     {
         $data = $this->cartService->getAll($request->getLocale(), true);
-        if($data['total'] <= 0) {
+        if ($data['total'] <= 0) {
             return $this->redirectToRoute('index');
         }
 
