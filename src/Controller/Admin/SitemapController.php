218d4dde7a7be68f03bea55fa02f009c5d31eee0
eugenpopov <zheny1995q@gmail.com>
2020-05-12 18:33:39 +0300

    Added robots.txt updating

--- src/Controller/Admin/SitemapController.php
+++ src/Controller/Admin/SitemapController.php
@@ -4,8 +4,11 @@
 namespace App\Controller\Admin;
 
 
+use App\Form\RobotsTxtForm;
+use App\Service\RobotsTxtService;
 use App\Service\SitemapService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class SitemapController extends AbstractController
 {
@@ -13,14 +16,39 @@ class SitemapController extends AbstractController
      * @var SitemapService
      */
     private $sitemapService;
+    /**
+     * @var RobotsTxtService
+     */
+    private $robotsTxtService;
 
     /**
      * SitemapController constructor.
      * @param SitemapService $sitemapService
+     * @param RobotsTxtService $robotsTxtService
      */
-    public function __construct(SitemapService $sitemapService)
+    public function __construct(SitemapService $sitemapService, RobotsTxtService $robotsTxtService)
     {
         $this->sitemapService = $sitemapService;
+        $this->robotsTxtService = $robotsTxtService;
+    }
+
+    public function uploadRobotsTxt(Request $request)
+    {
+        $form = $this->createForm(RobotsTxtForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->robotsTxtService->update($form->getData()['image']);
+            return $this->redirectToRoute('admin_settings_index');
+        }
+
+        return $this->render(
+            'admin/form.html.twig',
+            [
+                'dont_show_lang_block' => true,
+                'form' => $form->createView()
+            ]
+        );
     }
 
     public function generate()
@@ -28,4 +56,5 @@ class SitemapController extends AbstractController
         $this->sitemapService->generate();
         die('Ready');
     }
+
 }
